29019774c56faaa5c1737a692c8d4f606ea87a60
lucas-zortea <61652138+lucas-zortea@users.noreply.github.com>
2021-12-21 17:49:09 -0300

    Criação de Tabelas no Banco de Dados e implementação do crud em instrutores

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('calendariocorporativo', 'root', '123', { // (nome do banco, usuário, senha)
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = connection;
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express'); // Importando o Express
 const app = express(); // Instanciando o express
 const bodyParser = require('body-parser'); // Importando o Body-Parser
-const connection = require('./database/database') // Importando a Conexão com o Banco de dados
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -13,13 +12,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Database
-connection.authenticate()
-    .then(() => {
-        console.log('Conexão feita com sucesso');
-    }).catch((erro) => {
-        console.log(erro);
-    })
 
 app.get('/', (req, res) => { // Criando uma rota principal
     res.render('index');
